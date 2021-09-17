f9f423b6381d5c873810a331e5a0eb1735afaf8c
yokoyamahirokazu <h_yokoyama@side-creative.com>
2021-09-17 14:17:36 +0900

    言語変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
     htmlAttrs: {
       lang: "ja"
     },
+
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
